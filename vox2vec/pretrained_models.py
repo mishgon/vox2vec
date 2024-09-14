6c661da183daca7d0ea983c804ea00fc34d7d07f
Mikhail Goncharov <mig0nch@yandex.ru>
2024-09-14 08:10:15 +0000

    simclr_dim32_*nlst

--- vox2vec/pretrained_models.py
+++ vox2vec/pretrained_models.py
@@ -14,23 +14,25 @@ class Vox2VecForScreener(nn.Module):
     def __init__(
             self,
             name: Literal[
-                'moco_dim32_nlst',
                 'simclr_dim32_nlst',
-                'moco_dim128_nlst',
-                'moco_dim32_0.1nlst',
-                'moco_dim32_0.01nlst',
-                'moco_dim32_all'
+                'simclr_dim32_all',
+                'simclr_dim32_nlst_lr0.01',
+                'simclr_dim32_nlst_lr0.01_ln',
+                'moco_dim32_nlst',
+                'simclr_dim128_nlst',
+                'simclr_masked_dim32_all'
             ],
-            revision: str = '44add8acb1d9d307d7ea31c702ae20a0a6d5f80e'
+            # revision: str = 
     ):
         super().__init__()
 
-        self.out_channels = int(name.split('_')[1][len('dim'):])
+        self.out_channels = 32 if name != 'simclr_dim128_nlst' else 128
         self.backbone = UNet3d(
             in_channels=1,
             stem_stride=1,
             out_channels=self.out_channels,
-            fpn_out_channels=(32, 64, 128, 256),
+            fpn_out_channels=(16, 64, 256, 1024),
+            fpn_hidden_factors=(1.0, 1.0, 4.0, 4.0),
             fpn_depths=((1, 1), (2, 1), (4, 1), 8),
             stem_kernel_size=7,
             stem_padding=3,
@@ -41,12 +43,12 @@ class Vox2VecForScreener(nn.Module):
         )
         self.name = name
 
-        weights_path = hf_hub_download(
-            repo_id='mishgon/vox2vec',
-            filename=f'{name}.pt',
-            revision=revision
-        )
-        self.load_state_dict(torch.load(weights_path))
+        # weights_path = hf_hub_download(
+        #     repo_id='mishgon/vox2vec',
+        #     filename=f'{name}.pt',
+        #     revision=revision
+        # )
+        # self.load_state_dict(torch.load(weights_path))
 
     def forward(self, x: torch.Tensor) -> List[torch.Tensor]:
         return self.backbone(x)
